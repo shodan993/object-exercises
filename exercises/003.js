a6c78d724725d9e2ac60111038f3ec0b388f2df3
shodan993 <paulnstanley@gmail.com>
2018-07-26 08:44:15 -0400

    complete

--- exercises/003.js
+++ exercises/003.js
@@ -13,13 +13,23 @@ Function's return value (output):
 
 Do not change the input string. Assume that all elements in the array will be of type 'string'.
 
-Note that the input may have a different number of elements than the given sample. 
+Note that the input may have a different number of elements than the given sample.
 For instance, if the input had 6 values instead of 4, your code should flexibly accommodate that.
 
 Starter Code:
 */
 
 function fromListToObject(array) {
-  // your code here
-
+  let newObj = {};
+  array.forEach(function(element) {
+    newObj[element[0]] = element[1];
+  });
+  return newObj;
+  // console.log(newObj);
 }
+
+
+
+var arrayOfArrays = [['make', 'Ford'], ['model', 'Mustang'], ['year', '1964']];
+
+fromListToObject(arrayOfArrays);
